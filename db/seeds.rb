63baf747c5dd9bc56025e1083f704f19b316578f
Ty Brooks <brooks85.ty@gmail.com>
2014-04-10 11:52:25 -0700

    Better Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,49 +6,32 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-users = User.create!([
-  {email: 'ty@ty-brooks.com', password: 'pass'}
-  {email: 'guest@ty-brooks.com', password: 'password'}
+guest = User.create!(email: 'guest@ty-brooks.com', password: 'password')
+ty = User.create!(email: 'ty@ty-brooks.com', password: 'pass')
+
+# Sample board 1
+sample_board = guest.boards.build(
+  title: "Example Board"
+)
+
+sample_board.lists.build(
+  [
+    {rank: 1, title: "To Do", }, #1
+    {rank: 2, title: "Doing"}, #2
+    {rank: 3, title: "Done"} #3
+  ]
+)
+
+sample_board.lists[0].cards.build([
+  {rank: 1, title: "Clean dishes"},
+  {rank: 2, title: "Mop kitchen"}
+])
+sample_board.lists[1].cards.build([
+  {rank: 1, title: "Fix back door"}
+])
+sample_board.lists[2].cards.build([
+  {rank: 1, title: "Water plants"},
+  {rank: 2, title: "Fold sheets"}
 ])
 
-boards = Board.create!([
-  {title: "Home"}, #1
-  {title: "Work"} #2
-])
-
-board_assignments = BoardAssignment.create!([
-  {user_id: 1, board_id: 1},
-  {user_id: 1, board_id: 2}
-])
-
-lists = List.create!([
-  #Home: board 1
-  {board_id: 1, rank: 1, title: "To Do", }, #1
-  {board_id: 1, rank: 2, title: "Doing"}, #2
-  {board_id: 1, rank: 3, title: "Done"}, #3
-  #Work: board 2
-  {board_id: 2, rank: 1, title: "Urgent", }, #4
-  {board_id: 2, rank: 2, title: "Important"}, #5
-  {board_id: 2, rank: 3, title: "Optional"} #6
-])
-
-cards = Card.create!([
-  #To Do: List 1 on Home
-  {list_id: 1, rank: 1, title: "Clean dishes"},
-  {list_id: 1, rank: 2, title: "Mop kitchen"},
-  #Doing: List 2 on Home
-  {list_id: 2, rank: 1, title: "Fix back door"},
-  #Done: List 3 on Home
-  {list_id: 3, rank: 1, title: "Water plants"},
-  {list_id: 3, rank: 2, title: "Fold sheets"},
-
-  #Urgent: List 4 on Work
-  {list_id: 4, rank: 1, title: "Prepare earnings spreadsheet"},
-  {list_id: 4, rank: 2, title: "Build sales presentation"},
-  #Important: List 5 on Work
-  {list_id: 5, rank: 1, title: "Find new accountant"},
-  {list_id: 5, rank: 2, title: "Look into bigger office space"},
-  #Optional: List 6 on Work
-  {list_id: 6, rank: 1, title: "Review workflows"},
-  {list_id: 6, rank: 2, title: "Redecorate conference room"}
-])
+sample_board.save
